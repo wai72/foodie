02d1ef342c71106a111ac601fab806953f60ea15
Aye Thant Thant Thaw <ayethantthantthaw@abank.com.mm>
2024-11-17 21:44:05 +0630

    otp states

--- lib/app/modules/otp/otp_view.dart
+++ lib/app/modules/otp/otp_view.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:foodie/app/src/app_spacings.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
@@ -14,29 +16,44 @@ class OtpView extends StatefulWidget {
 }
 
 class _OtpViewState extends State<OtpView> {
+  final TextEditingController _otpController = TextEditingController();
   late String receivedPhoneNumber;
   bool _isEnableContinue = false;
-  final TextEditingController _otpController = TextEditingController();
   final String _validOtp = "123456";
-  String _statusMessage = "";
+  Timer? _timer;
+  int _remainingTime = 10;
+  bool _isResend = false;
+  bool _isCorrectOTP = true;
 
-// For demonstration; in real scenarios, this comes from backend.
-
-  void _checkOtp(String otp) {
-    setState(() {
-      if (otp == _validOtp) {
-        _statusMessage = "OTP Verified Successfully!";
-        _isEnableContinue = true;
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_remainingTime > 0) {
+        setState(() {
+          _remainingTime--;
+        });
       } else {
-        _statusMessage = "Invalid OTP. Please try again.";
+        timer.cancel();
       }
     });
+  }
+
+  void _resendOtp() {
+    setState(() {
+      _remainingTime = 10;
+    });
+    _startTimer();
   }
 
   @override
   void initState() {
     super.initState();
     receivedPhoneNumber = widget.phoneNumber;
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel(); // Cancel timer when widget is disposed
+    super.dispose();
   }
 
   @override
@@ -83,7 +100,9 @@ class _OtpViewState extends State<OtpView> {
               ),
               Container(
                 padding: const EdgeInsets.only(
-                    top: AppSpacing.l, bottom:AppSpacing.xs,left: AppSpacing.s),
+                    top: AppSpacing.l,
+                    bottom: AppSpacing.xs,
+                    left: AppSpacing.s),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(2),
                   color: Colors.white,
@@ -92,29 +111,90 @@ class _OtpViewState extends State<OtpView> {
                     width: 1.0, // Border width
                   ),
                 ),
-                child: PinCodeTextField(
-                  textStyle:  TextStyle(fontSize: 15,fontWeight: FontWeight.w300),
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  appContext: context,
-                  pinTheme: PinTheme(
-                      activeColor: AppColors.whiteColor,
-                      inactiveColor: AppColors.strokeColor,
-                      selectedColor: AppColors.strokeColor,
-                      fieldHeight: 20,
-                      fieldOuterPadding:
-                          const EdgeInsets.only(right: AppSpacing.xs),
-                      fieldWidth: 15),
-                  length: 6,
-                  validator: (otp) {
-                    if (otp == _validOtp) {
-                      _statusMessage = "OTP Verified Successfully!";
-                      _isEnableContinue = true;
-                    } else {
-                      _statusMessage = "Invalid OTP. Please try again.";
-                    }
-                  },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: PinCodeTextField(
+                        controller: _otpController,
+                        textStyle: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w300),
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        appContext: context,
+                        pinTheme: PinTheme(
+                            activeColor: AppColors.whiteColor,
+                            inactiveColor: AppColors.strokeColor,
+                            selectedColor: AppColors.strokeColor,
+                            fieldHeight: 20,
+                            fieldOuterPadding:
+                                const EdgeInsets.only(right: AppSpacing.xs),
+                            fieldWidth: 15),
+                        length: 6,
+                        validator: (otp) {
+                          if (otp == _validOtp) {
+                            _isEnableContinue = true;
+                            _isCorrectOTP = true;
+                          } else {
+                            _isCorrectOTP = false;
+                          }
+                        },
+                      ),
+                    ),
+                    if (_isResend && _remainingTime > 0)
+                      Padding(
+                          padding: const EdgeInsets.only(right: AppSpacing.s),
+                          child: Text(
+                            "${_remainingTime}s",
+                            style: const TextStyle(
+                              color: AppColors.textColor,
+                            ),
+                          ))
+                  ],
                 ),
               ),
+              const SizedBox(
+                height: AppSpacing.xs,
+              ),
+              if ((!_isResend &&_isCorrectOTP)||(!_isEnableContinue))
+                InkWell(
+                  child: RichText(
+                    text: const TextSpan(children: [
+                      TextSpan(
+                        text: "Didn't received?",
+                        style: TextStyle(color: AppColors.textColor),
+                      ),
+                      TextSpan(
+                        text: "Resend",
+                        style: TextStyle(color: AppColors.linkColor),
+                      )
+                    ]),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _isResend = true;
+                      _startTimer();
+                    });
+                  },
+                ),
+              if (!_isCorrectOTP)
+                InkWell(
+                  child: RichText(
+                    text: const TextSpan(children: [
+                      TextSpan(
+                        text: "Wrong OTP.",
+                        style: TextStyle(color: AppColors.textColor),
+                      ),
+                      TextSpan(
+                        text: "TryAgain",
+                        style: TextStyle(color: AppColors.linkColor),
+                      )
+                    ]),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _otpController.clear();
+                    });
+                  },
+                ),
               const Spacer(),
               Center(
                 child: ElevatedButton(
